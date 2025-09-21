a10527302f9cac024a8431ebc38a83e076f8d926
HAOEN LI <subaru05909@gmail.com>
2025-09-22 05:59:07 +0800

    5.new Unity Input System!!!

--- FYP/Assets/Player.cs
+++ FYP/Assets/Player.cs
@@ -2,17 +2,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private PlayerinputSet input;
     private StateMachine stateMachine;
     public Player_IdleState idleState { get; private set; }
     public Player_MoveState moveState { get; private set; }
 
+    public Vector2 moveInput {  get; private set; }
+
     private void Awake()
     {
         stateMachine = new StateMachine();
+        input = new PlayerinputSet();
 
         idleState = new Player_IdleState(this, stateMachine, "idle");
         moveState = new Player_MoveState(this, stateMachine, "move");
     }
+
+    private void OnEnable()
+    {
+        input.Enable();
+
+        input.Player.Movement.performed += contex => moveInput = contex.ReadValue<Vector2>();
+        input.Player.Movement.canceled += contex => moveInput = Vector2.zero;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
 
     private void Start()
     {
