4260ec2603a2e46a862a970bb842ff4106b54439
Smile-e <yixiao.wang030824@gmail.com>
2025-10-27 22:36:06 +0800

    finish menu

---
+++ FYP/Assets/Menu/MenuUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene"); 
+    }
+
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; 
+#endif
+    }
+}
