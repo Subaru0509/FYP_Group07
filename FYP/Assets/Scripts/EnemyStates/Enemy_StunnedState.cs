1d57caff17aca07a3923b7cd9844096477bfc553
HAOEN LI <subaru05909@gmail.com>
2025-12-01 06:13:52 +0800

    attack alert

--- FYP/Assets/Scripts/EnemyStates/Enemy_StunnedState.cs
+++ FYP/Assets/Scripts/EnemyStates/Enemy_StunnedState.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class Enemy_StunnedState : EnemyState
 {
+
+    private Enemy_VFX vfx;
     public Enemy_StunnedState(Enemy enemy, StateMachine stateMachine, string animBoolName) : base(enemy, stateMachine, animBoolName)
     {
+        vfx = enemy.GetComponent<Enemy_VFX>();
     }
 
 
     public override void Enter()
     {
         base.Enter();
+
+        vfx.EnableAttackAlert(false);
+        enemy.EnableCounterWindow(false);
 
         stateTimer = enemy.stunnedDuration;
         rb.velocity = new Vector2(enemy.stunnedVelocity.x * -enemy.facingDir, enemy.stunnedVelocity.y);
